18ebecf3b54cb914b2ce052ce53a6e0f31449fae
NanoClem <clemdecoop.pro@gmail.com>
2022-08-07 03:18:27 +0200

    [feat] add scraping route

--- app/main.py
+++ app/main.py
@@ -1,14 +1,32 @@
-from fastapi import FastAPI
+import uvicorn
+import requests as rq
+from fastapi import FastAPI, HTTPException, status
+
+from models import WebsiteNames
+from scraper import Scraper
 
 
 app = FastAPI()
+scraper = Scraper()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    return {"Hello": "planet"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/jobs/scrape/{website}", status_code=status.HTTP_200_OK)
+async def scrape_jobs(website: WebsiteNames, q: str | None = None):
+    """d"""
+    try:
+        result = scraper.scrape(website)
+    except rq.HTTPError:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail='An error occured while scraping data.',
+        )
+    return result
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=5000, reload=True)
