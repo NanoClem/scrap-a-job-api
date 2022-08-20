e632ab31fe18e6a323d14353e3df038b24c25105
NanoClem <clemdecoop.pro@gmail.com>
2022-08-20 04:10:34 +0200

    [fix] fix model declaration

---
+++ src/api/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String
+
+from api.database import Base
+
+
+class JobModel(Base):
+    __tablename__ = 'job_adds'
+
+    id = Column(Integer, primary_key=True, index=True)
+    source_id = Column(String, unique=True, index=True, nullable=False)
+    title = Column(String)
+    slug = Column(String)
+    url = Column(String)
+    company = Column(String)
+    source_website = Column(String)
+    employment_type = Column(String)
+    employment_rate = Column(Integer)
+    category = Column(String)
+    extent = Column(String)
+    description = Column(String)
+    location = Column(String)
+    publication_date = Column(String)
