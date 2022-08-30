8db860eb8afca8ecca6ab45198122dfe4eed1b46
NanoClem <clemdecoop.pro@gmail.com>
2022-08-30 04:39:27 +0200

    [fix] remove db creation

--- src/api/main.py
+++ src/api/main.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 
 from api.database import Base, engine
 from api.routers.job_add_router import router as job_add_router
-from api import scraping
-
-# Init database
-Base.metadata.create_all(bind=engine)
 
 # Init modules
+from api import scraping
 scraping.init_module()
 
 # Init app
